170b33665926db3384f70faf319bcf2597f0004a
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-01-23 23:37:44 +0530

    Added keyboard press events

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ buttons.forEach(button => {
         if (allowedChars.indexOf(value) !== -1) {
 
             display.value += value;
-        } else if (value === 'c') {
+        } else if (value === 'C') {
 
             display.value = '';
         } else if (value === '=') {
@@ -36,4 +36,29 @@ buttons.forEach(button => {
 
 
 
+document.addEventListener("keydown", (eve) => {
+    const key = eve.key;
 
+    if (key === 'Escape' || key === 'c' || key === 'C') {
+        display.value = '';
+    } else if (key === '%') {
+        try {
+            display.value = new Function("return " + display.value + " / 100")() + "%";
+        } catch (error) {
+            display.value = 'Error';
+        }
+    } else if (key === 'Backspace') {
+        display.value = display.value.slice(0, -1);
+    } else if (allowedChars.indexOf(key) !== -1) {
+        display.value += key;
+    } else if (key === '=' || key === "Enter") {
+
+        try {
+
+            display.value = new Function("return " + display.value)();
+        } catch (error) {
+
+            display.value = 'Error';
+        }
+    }
+});
